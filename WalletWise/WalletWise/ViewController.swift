a9e33ee8e52f0db3c09c149376ba9255d3630c2c
Daize Njounkeng <njounkengdaizem@berea.edu>
2023-04-08 00:09:26 -0400

    added launchscreen and icon

---
+++ WalletWise/WalletWise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletWise
+//
+//  Created by Daize Njounkeng on 4/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
