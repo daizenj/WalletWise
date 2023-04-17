694740a5a538a240e4fad1f12e7b92baaceedb9f
TECNA7 <klamichhan@brynmawr.edu>
2023-04-16 23:36:09 -0400

    update

--- WalletWise/WalletWise/AppDelegate.swift
+++ WalletWise/WalletWise/AppDelegate.swift
@@ -5,19 +5,45 @@
 //  Created by Daize Njounkeng on 4/7/23.
 //
 
+import ParseSwift
 import UIKit
+
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+       
+        // https://github.com/parse-community/Parse-Swift/blob/main/ParseSwift.playground/Sources/Common.swift
+        ParseSwift.initialize(applicationId: "Z8WYOJ22LRuChlemsxLot9eJefWwBlmC6lNPPWeB",
+                              clientKey: "gNwm5gVjtMtUQyNxnKGA5hX6cNhfbtN2weuPr5jc",
+                              serverURL: URL(string: "https://parseapi.back4app.com")!)
+
+       
+        // https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L121
+
+        // Instantiate the test parse object
+        var score = GameScore()
+        score.playerName = "Kingsley"
+        score.points = 13
+
+        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+        // If no callbackQueue is specified it returns to main queue.
+        score.save { result in
+            switch result {
+            case .success(let savedScore):
+                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+            case .failure(let error):
+                assertionFailure("Error saving: \(error)")
+            }
+        }
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -26,11 +52,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+       
     }
+}
 
 
+// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
+// Create your own value type `ParseObject`.
+struct GameScore: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    // All custom properties must be optional.
+    var playerName: String?
+    var points: Int?
+}
+
+
+extension GameScore {
+
+    
+    init(playerName: String, points: Int) {
+        self.playerName = playerName
+        self.points = points
+    }
 }
 
