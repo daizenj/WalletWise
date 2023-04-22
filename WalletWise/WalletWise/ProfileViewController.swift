cf8e3d1b430749deea086ced8347cfdff2eec305
Daize Njounkeng <njounkengdaizem@berea.edu>
2023-04-22 01:13:18 -0400

    Profile page complete

--- WalletWise/WalletWise/ProfileViewController.swift
+++ WalletWise/WalletWise/ProfileViewController.swift
@@ -8,23 +8,13 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.rgb(red: 27, green: 29, blue: 47)
+//        myTitle.text = "My Profile"
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
