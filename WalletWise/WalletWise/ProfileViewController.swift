8b182f97d258e6effbce4aee46c3bacb47232ff3
Daize Njounkeng <njounkengdaizem@berea.edu>
2023-04-22 00:37:02 -0400

    Added profile skeleton

---
+++ WalletWise/WalletWise/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  WalletWise
+//
+//  Created by Daize Njounkeng on 4/21/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.rgb(red: 27, green: 29, blue: 47)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- WalletWise/WalletWise/ViewController.swift
+++ WalletWise/WalletWise/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        performSegue(withIdentifier: "summaryViewSegue", sender: nil)
+//        performSegue(withIdentifier: "summaryViewSegue", sender: nil)
+        performSegue(withIdentifier: "profileViewSegue", sender: nil)
         // Do any additional setup after loading the view.
     }
 
