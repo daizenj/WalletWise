c1823582bc65c34d9ed0125a83a4be5d0eb8dd1f
Shokhinabonu <50881414+Shokhinabonu@users.noreply.github.com>
2023-04-30 10:44:00 -0400

    main screen updates

---
+++ WalletWise/WalletWise/CalendarCell.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarCell.swift
+//  WalletWise
+//
+//  Created by Shokhinabonu Tojieva on 4/18/23.
+//
+
+import Foundation
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+    
+}
